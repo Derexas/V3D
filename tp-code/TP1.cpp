be2798227fe92ab152e292bf4b84c7be22a00548
derexas <derexas@gmail.com>
2017-05-14 19:39:47 +0200

    droite epipolaire : check

--- tp-code/TP1.cpp
+++ tp-code/TP1.cpp
@@ -9,17 +9,20 @@
 
 using namespace std ;
 
-
+void process_x2_y2(double& x2, double& y2, double x1, double y1, double Z, 
+  double r1, double r2, double r3,
+  double tx, double ty, double tz)
+{
+  x2 = (Z*(r1*x1 + 0*y1 + 0*1) + tx) / (Z*(0*x1 + 0*y1 + r3*1) + tz);
+  y2 = (Z*(0*x1 + r2*y1 + 0*1) + ty) / (Z*(0*x1 + 0*y1 + r3*1) + tz);
+}
 
 int main()
 {
   vpImage<unsigned char> Ig(300,400,0);
   vpImage<unsigned char> Id(300,400,0);
   vpImage<vpRGBa> Iimage(876,1200);
-  
- 
-  vpImageIo::read(Iimage,"../data/big-sleep.jpg") ;
-  
+  vpImageIo::read(Iimage,"../data/big-sleep.jpg");
 
   double L = 0.600 ;
   double l = 0.438;
@@ -50,16 +53,20 @@ int main()
 
   vpImageSimulator sim;
   sim.init(Iimage, X);
-  vpCameraParameters cam(800.0, 800.0, 200, 150);
-  
-
+  double px = 800; double py = 800;
+  int u0 = 200; int v0 = 150;
+  vpCameraParameters cam(px, py, u0, v0);
 
 
   cam.printParameters() ;
 
 
+  double r1, r2, r3;
+  r1 = 1; r2 = 1; r3 = 1;
+  double tx = 0.7; double ty = 0; double tz = 2;
+
   // I1g
-  vpHomogeneousMatrix  gMo(0, 0, 2, vpMath::rad(0),vpMath::rad(0),0);
+  vpHomogeneousMatrix  gMo(tx, ty, 2+tz, vpMath::rad(0),vpMath::rad(0),0);
   sim.setCameraPosition(gMo);
   sim.getImage(Ig,cam);
   cout << "Image I1g " << endl ;
@@ -67,7 +74,7 @@ int main()
   cout << gMo << endl ;
 
   // I1d
-  vpHomogeneousMatrix  dMo(0.1, 0, 2, vpMath::rad(0),vpMath::rad(0),0);
+  vpHomogeneousMatrix  dMo(0, 0, 2, vpMath::rad(0),vpMath::rad(0),0);
 //  vpHomogeneousMatrix dMo(0.1, 0, 1.9, vpMath::rad(5),vpMath::rad(5),vpMath::rad(5));
   sim.setCameraPosition(dMo);
   sim.getImage(Id,cam);
@@ -97,7 +104,24 @@ int main()
 
       // Calcul du lieu geometrique
       //....
+      double u = pd.get_u();
+      double v = pd.get_v();
 
+      double x1 = (u - u0) / px;
+      double y1 = (v - v0) / py;
+      double Z = 1;
+      double x2, y2;
+
+      vpImagePoint p1, p2;
+      process_x2_y2(x2, y2, x1, y1, Z, r1, r2, r3, tx, ty, tz);
+      p1.set_u(x2 * px + u0); p1.set_v(y2 * py + v0);
+      Z = 1000;
+      process_x2_y2(x2, y2, x1, y1, Z, r1, r2, r3, tx, ty, tz);
+      p2.set_u(x2 * px + u0); p2.set_v(y2 * py + v0);
+      std::cout << pd << " | " << p2 << std::endl;
+
+
+      vpDisplay::displayDotLine(Ig, p1, p2, vpColor::red);
 
       // Affichage dans Ig
       
