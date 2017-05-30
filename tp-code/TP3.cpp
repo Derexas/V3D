4d9f872aab82faf97f24fb31705ace386f2c14b9
derexas <derexas@gmail.com>
2017-05-30 19:19:43 +0200

    finished tp3+4

--- tp-code/TP3.cpp
+++ tp-code/TP3.cpp
@@ -18,7 +18,7 @@ void DLT(unsigned int n,
 {
 	H21 = vpMatrix(3,3);
 	
-	// NBPTMIN points son necessaire ; remplace le 1e6 par ce nombre
+	// NBPTMIN points sont necessaires ; remplace le 1e6 par ce nombre
 	#define NBPTMIN 4
 	if(n < NBPTMIN )
 	{
@@ -66,11 +66,11 @@ int main()
 	vpImage<unsigned char> I1;
 	vpImage<unsigned char> I2;
 	vpImage<vpRGBa> Iimage(876,1200);
-	
+	vpImage<unsigned char> I3;
  
 	vpImageIo::read(I1,"../data/I1.pgm") ;
 	vpImageIo::read(I2,"../data/I2.pgm") ;
-
+	vpImageIo::read(I3,"../data/I2.pgm") ;
 
 
 	vpCameraParameters cam(800.0, 800.0, 200, 150);
@@ -80,13 +80,17 @@ int main()
 	vpDisplay::display(I1) ;
 	vpDisplay::flush(I1) ;
 
-	vpDisplayX d2(I2,450,10,"I2") ;
+	vpDisplayX d2(I2,700,10,"I2") ;
 	vpDisplay::display(I2) ;
 	vpDisplay::flush(I2) ;
 
 	int nb = 5;
 	vpImagePoint p1[nb], p2[nb];
 	
+	
+
+	//////// Initialisation interactive des points 
+
 	// clicker 5 point sur l'image I2 ; recuperer leur coordonnees
 	for(unsigned int i=0; i<nb; i++)
 	{
@@ -99,8 +103,9 @@ int main()
 		vpDisplay::displayCross(I1,p1[i],10,vpColor::blue) ;
 		vpDisplay::displayCharString(I1,p1[i],s,vpColor::red) ;
 		vpDisplay::flush(I1) ;
+		cout << "p1[" << i << "] =" << p1[i]<< "; " << endl;
 	}
-	
+		
 	// clicker 5 point sur l'image I1 ; recuperer leur coordonnees
 	// faites attention a les clicker dans le meme ordre
 	for(unsigned int i=0; i<nb; i++)
@@ -114,19 +119,42 @@ int main()
 		vpDisplay::displayCross(I2,p2[i],10,vpColor::green) ;
 		vpDisplay::displayCharString(I2,p2[i],s,vpColor::red) ;
 		vpDisplay::flush(I2) ;
+		cout << "p2[" << i << "] =" << p2[i]<< "; " << endl;
 	}
 	
 
-	// Calculer l'homographie
+	//////// Initialisation automatique des points
+	
+/*
+	p1[0].set_uv(135.0902394, 44.39594843 ); 
+	p1[1].set_uv(464.2350598, 185.749004  ); 
+	p1[2].set_uv(298.9828851, 236.6234719 ); 
+	p1[3].set_uv(49.95131579, 339.1552632 ); 
+	p1[4].set_uv(432.2818991, 422.9080119 ); 
+	p2[0].set_uv(122.9570552, 126.5562372 ); 
+	p2[1].set_uv(453.5905797, 61.28985507 ); 
+	p2[2].set_uv(336.4578652, 198.3960674 ); 
+	p2[3].set_uv(199.1065574, 388.9815574 ); 
+	p2[4].set_uv(521.6608997, 278.432526  );
+	
+	for(unsigned int i=0; i<nb; i++)
+	{
+	  cout << "p1[" << i << "] =" << p1[i]<< "; " << endl;
+	  cout << "p2[" << i << "] =" << p2[i]<< "; " << endl;
+	}
+*/
+	//////// Calcul l'homographie
+	
 	vpMatrix H21 ;
 	DLT(nb, p1, p2, H21) ;
 
 	cout << "Homographie H21 : " << endl ; 
 	cout << H21 << endl ;
 
-	//Verification 
+	//////// Verification
+	
 	double residue = 0;
-	for (int i=0 ; i < nb ; i++) 
+        for (int i=0 ; i < nb ; i++) 
 	{
 		// Connaissant le formule permettant le transfert des points p2 dans p1
 		// Calculer les coordonnées des point p1 connaissant p2 et dHg
@@ -139,21 +167,65 @@ int main()
 		// afficher un cercle de rayon 10 fois cette erreur
 		double r ;
 		r = vpImagePoint::distance(p2_calcule, p2[i]);
-		cout << "point " << i << "	" << r << " " << p2_calcule << " " << p2[i] << endl ;
+		cout << "point " << i << " : " << p1[i] << " >>  " << r << " " << p2_calcule << " " << p2[i] << endl ;
 		double rayon ;
 		rayon = sqrt(r)*10 ; if (rayon < 10) rayon = 10 ;
 		vpDisplay::displayCircle(I2, p2_calcule, rayon, vpColor::green);
+		vpDisplay::displayCircle(I2, p2[i], rayon, vpColor::green);
+		vpDisplay::displayCircle(I1, p1[i], rayon, vpColor::green);
 	}
 
+	vpDisplay::flush(I1) ;
 	vpDisplay::flush(I2) ;
 	vpImage<vpRGBa> Ic ;
 	vpDisplay::getImage(I2,Ic) ;
 	vpImageIo::write(Ic,"resultat.jpg") ;
 
-	vpDisplay::getClick(I2) ;
+	//vpDisplay::getClick(I2) ;
 
-	vpDisplay::close(I2) ;
+	//vpDisplay::close(I2) ;
+	//vpDisplay::close(I1) ;
+	vpColVector v1(3), v2(3);
+
+	//////// Mosaique
+
+	
+	for (int i = 0; i<I1.getWidth(); i++){
+		for (int j = 0; j<I1.getHeight(); j++){
+	    		v1[0] = i; v1[1] = j; v1[2] = 1;
+	    		v2 = H21 * v1; v2 /= v2[2];
+	    		vpImagePoint p2_calcule; p2_calcule.set_uv(v2[0], v2[1]);
+	    		int ii = (int) p2_calcule.get_u(); int jj = (int) p2_calcule.get_v();
+	    		if (ii<I2.getWidth() && ii > 0 && jj<I2.getHeight() && jj > 0){
+	      			I3[(int)jj][(int)ii] = (1*I2[(int)jj][(int)ii]+ 1*I1[j][i])/2;
+	    		}
+	  	}
+	}
+	
+	vpDisplayX d3(I3,10,450,"I3") ;
+	vpDisplay::display(I3) ;
+	vpDisplay::flush(I3) ;
+	vpDisplay::getClick(I3) ;
+
+	//////// Affichage vérification (rayons des cercles fixes)
+/*	
+	for (int i=0 ; i < nb ; i++) 
+	{
+	  	vpDisplay::displayCircle(I3,p2[i],10,vpColor::green);
+	  	cout << p2[i] << " ===> ";
+	  	v1[0] = p1[i].get_u(); v1[1] = p1[i].get_v(); v1[2] = 1;
+	  	v2 = H21*v1; v2 /= v2[2];
+	  	vpImagePoint p2_calcule; p2_calcule.set_uv(v2[0], v2[1]);
+	  	vpDisplay::displayCircle(I3,p2_calcule,20,vpColor::red);
+	  	int ii = (int) p2_calcule.get_u(); int jj = (int) p2_calcule.get_v();
+	  	cout << ii << " " << jj << endl;
+	}
+
+	vpDisplay::flush(I3) ;
+	vpDisplay::getClick(I3) ;
+*/
 	vpDisplay::close(I1) ;
-
+	vpDisplay::close(I2) ;
+	vpDisplay::close(I3) ;
 	return 0;
 }
--- tp-code/TP4.cpp
+++ tp-code/TP4.cpp
@@ -76,6 +76,7 @@ void AfficheAppariement(vpImage<unsigned char> &I1,
 			p2[i].set_u(p2[i].get_u()+I1.getCols()) ;
 			vpDisplay::displayCharString(I,p1[i],s,vpColor::red) ;
 			vpDisplay::displayCharString(I,p2[i],s,vpColor::red) ;
+			cout<<p1[i]<<endl;
 			vpDisplay::displayLine(I,p1[i],p2[i],vpColor::yellow) ;
 			
 		} 
@@ -83,14 +84,147 @@ void AfficheAppariement(vpImage<unsigned char> &I1,
 	vpDisplay::flush(I) ;
 }
 
+void trans(vpImage<unsigned char>& I1, vpImage<unsigned char>& I2, vpMatrix H21, vpImage<unsigned char>& I3){
 
-
-void DLT()
-{
+for (int i = 0; i<I1.getWidth(); i++){
+	  for (int j = 0; j<I1.getHeight(); j++){
+	    	I3[j][i] = I2[j][i];
+	  }
+}
+vpColVector v1(3), v2(3);
+for(int i = 0; i<I1.getWidth(); i++){
+		for (int j = 0; j<I1.getHeight(); j++){
+	    		v1[0] = i; v1[1] = j; v1[2] = 1;
+	    		v2 = H21 * v1; v2 /= v2[2];
+	    		vpImagePoint p2_calcule; p2_calcule.set_uv(v2[0], v2[1]);
+	    		int ii = (int) p2_calcule.get_u(); int jj = (int) p2_calcule.get_v();
+	    		if (ii<I2.getWidth() && ii > 0 && jj<I2.getHeight() && jj > 0){
+	      			I3[(int)jj][(int)ii] = (1*I2[(int)jj][(int)ii]+ 1*I1[j][i])/2;
+	    		}
+	  	}
+}
 
 }
 
+void DLT(unsigned int n,
+	 vpImagePoint* p1,
+	 vpImagePoint* p2,
+	 vpMatrix &H21)
+{
 
+	H21 = vpMatrix(3,3);
+	
+	// NBPTMIN points son necessaire ; remplace le 1e6 par ce nombre
+	#define NBPTMIN 4
+	if(n < NBPTMIN )
+	{
+		cout << "there must be at least " << NBPTMIN <<	" points in the both images\n" <<endl	;
+		throw ;
+	}
+	
+	vpMatrix A(2*n,9);
+	for (int i = 0; i < n; i++) {
+		// Matrix A
+		A[2*i][0] = A[2*i][1] = A[2*i][2] = 0;
+		A[2*i][3] = -1*p1[i].get_u(); A[2*i][4] = -1*p1[i].get_v(); A[2*i][5] = -1*1;
+		A[2*i][6] = p2[i].get_v()*p1[i].get_u(); A[2*i][7] = p2[i].get_v()*p1[i].get_v(); A[2*i][8] = p2[i].get_v()*1;
+		A[2*i+1][0] = 1*p1[i].get_u(); A[2*i+1][1] = 1*p1[i].get_v(); A[2*i+1][2] = 1*1;
+		A[2*i+1][3] = A[2*i+1][4] = A[2*i+1][5] = 0;
+		A[2*i+1][6] = -p2[i].get_u()*p1[i].get_u(); A[2*i+1][7] = -p2[i].get_u()*p1[i].get_v(); A[2*i+1][8] = -p2[i].get_u()*1;
+	}
+//	std::cout << "A\n" << A << std::endl;
+
+	vpColVector D;
+	vpMatrix U, V;
+
+	A.svd(D, V);
+	U = A;
+
+	double minind = D[0]; int miniind = 0;
+	for (int i = 1; i < D.size(); i++)
+		if (D[i] < minind) {
+			minind = D[i];
+			miniind = i;
+		}
+	vpColVector h = V.getCol(miniind);
+//	cout << "V\n" << V << endl;
+//	cout << "D\n" << D << endl;
+	for (int i = 0; i < h.size(); i++) {
+		H21[i/3][i%3] = h[i];
+	}
+//	cout << "H21\n" << H21 << endl;
+	
+}
+
+void Ransac(unsigned int n,
+	 vpImagePoint* p1,
+	 vpImagePoint* p2,
+	 vpMatrix &H21){
+ srand(time(NULL));
+//cout <<"ransac"<<endl;
+ int nb = 0;
+ vpImagePoint res1[5], res2[5];
+ for(int k = 0; k<50; k++){
+	int r[5];
+	for (int i = 0; i<5; i++){
+		r[i] = -1;	
+	}
+	
+	for (int i = 0; i<5; i++){
+		int temp = rand()%n;
+//cout << temp << endl;
+		bool b = false;
+		for (int j = 0; j<5; j++){
+			if(r[j] == temp){
+				b = true;
+			}	
+		}
+		if(!b){
+			r[i] = temp;
+		}else{
+			i--;
+		}
+	}
+//cout << "fin rand" <<endl;
+	vpImagePoint point1[5], point2[5];
+	for (int i = 0; i<5; i++){
+		point1[i] = p1[r[i]];
+		point2[i] = p2[r[i]];	
+	}	
+	DLT(5, point1, point2, H21);
+//	cout << "Homographie H21 : " << endl ; 
+//	cout << H21 << endl ;
+	int d = 10;
+	int temp = 0;
+	for (int i=0 ; i < n ; i++) 
+	{
+		// Connaissant le formule permettant le transfert des points p2 dans p1
+		// Calculer les coordonnées des point p1 connaissant p2 et dHg
+		vpColVector v1(3), v2(3);
+		v1[0] = p1[i].get_u(); v1[1] = p1[i].get_v(); v1[2] = 1;
+		v2 = H21 * v1; 
+//cout <<i<<" "<< v2[2]<<endl;
+		v2 /= v2[2];
+		vpImagePoint p2_calcule; p2_calcule.set_uv(v2[0], v2[1]);
+		// en deduire l'erreur sur commise sur chaque point et 
+		// afficher un cercle de rayon 10 fois cette erreur
+		double r ;
+		r = vpImagePoint::distance(p2_calcule, p2[i]);
+//cout <<"r = "<<r<<endl;
+		if(r<d){
+			temp++;
+		}
+	}
+	if(temp>nb){
+		nb = temp;
+		for (int i=0 ; i < 5 ; i++){
+			res1[i] = point1[i];
+			res2[i] = point2[i];
+		}
+	}
+ }
+DLT(5, res1, res2, H21);	
+}
 
 int main()
 {
@@ -174,12 +308,9 @@ int main()
 	surf.setDescriptorType(vpKeyPointSurf::extendedDescriptor) ; 	
 	//First grab the reference image Irefrence
 	surf.setHessianThreshold(10000) ;
-
 	//Build the reference SURF points.
 	surf.buildReference(I1);
-
 	//Then grab another image which represents the current image Icurrent
-
 	//Match points between the reference points and the SURF points computed in the current image.
 	int nb =	surf.matchPoint(I2);
 	cout << "Nombre de points mis en correspondance " <<	nb <<	endl ;
@@ -187,11 +318,16 @@ int main()
 	surf.display(I1, I2);*/
 	int nb = 16;
 	vpImagePoint p1[nb], p2[nb];
-
+	vpImage<unsigned char> I3(300,400,0);
 	if(nb >= 0){ // ... add paired points to vectPts
 		
 		getCorrespondances(p1, p2);
-		AfficheAppariement(I1,	I2, I, p1, p2) ;
+		cout << "begin" << endl;
+		vpMatrix H21 ;
+		DLT(nb, p1, p2, H21);
+		//Ransac(nb, p1, p2, H21);
+		cout << "Homographie H21 : " << endl ; 
+		cout << H21 << endl ;
 		for(unsigned int i=0; i<nb; i++)
 		{
 			//surf.getMatchedPoints(i, p1[i], p2[i]);
@@ -201,16 +337,55 @@ int main()
 			cout <<	p2[i].get_u() <<"	" << p2[i].get_v() << endl;
 			vpDisplay::displayCharString(I1, p1[i], s, vpColor::yellow) ;
 			vpDisplay::displayCharString(I2, p2[i], s, vpColor::green) ;
+
+			
 		}
+		for (int i = 0; i<nb; i++){
+			// Connaissant le formule permettant le transfert des points p2 dans p1
+			// Calculer les coordonnées des point p1 connaissant p2 et dHg
+			vpColVector v1(3), v2(3);
+			v1[0] = p1[i].get_u(); v1[1] = p1[i].get_v(); v1[2] = 1;
+			v2 = H21 * v1; v2 /= v2[2];
+			vpImagePoint p2_calcule; p2_calcule.set_uv(v2[0], v2[1]);
+
+			// en deduire l'erreur sur commise sur chaque point et 
+			// afficher un cercle de rayon 10 fois cette erreur
+			double r ;
+			r = vpImagePoint::distance(p2_calcule, p2[i]);
+			cout << "point " << i <<  " r = " << r << " p_calcul " << p2_calcule << " p = " << p2[i] << endl ;
+			double rayon ;
+			rayon = sqrt(r)*10 ; if (rayon < 10) rayon = 10 ;
+			vpDisplay::displayCircle(I2, p2_calcule, rayon, vpColor::green);
+			vpDisplay::displayCross(I2,p2[i],10,vpColor::red) ;
+			vpDisplay::displayLine(I2,p2_calcule,p2[i],vpColor::yellow) ;
+		}
+		trans (I1,I2,H21,I3);
 	}
 	
+	
 
-	/*vpDisplay::display(I1) ;
+
+/*
+
+	vpDisplay::display(I1) ;
 	vpDisplay::display(I2) ;
+//	vpDisplay::display(I) ;
 	vpDisplay::flush(I1) ;
-	vpDisplay::flush(I2) ;*/
+	vpDisplay::flush(I2) ;
+//	vpDisplay::flush(I) ;
+*/
 
-	vpDisplay::getClick(I1) ;
+	vpDisplay::flush(I2) ;
+  	vpImage<vpRGBa> Ic ;
+  	vpDisplay::getImage(I2,Ic) ;
+
+	vpDisplay::getClick(I2) ;
+
+	vpDisplayX d3(I3,10,450,"I3") ;
+	vpDisplay::display(I3) ;
+	vpDisplay::flush(I3) ;
+	vpDisplay::getClick(I3) ;
+	AfficheAppariement(I1,	I2, I, p1, p2) ;
 
 	cout << "end" << endl;
 	return 0;
